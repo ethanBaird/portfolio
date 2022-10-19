d56b9fc429242b4007213d297b8f1a24bde55901
Ethan <ethanbaird94@gmail.com>
2022-10-19 12:59:45 +0100

    adds icons, need to fix sizing issue

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
+import { useState } from "react";
 import styled from "styled-components";
+import Project from "./containers/Project";
 import Welcome from "./containers/Welcome";
 
 function App() {
+
+  const [tracksideData, setTracksideData] = useState({
+    name: 'trackside',
+    img: '/trackside.png',
+    github: 'to add',
+  })
+
+  const [stockData, setStockData] = useState({
+    name: 'Financial Gains House',
+    img: '/fgh.jpeg',
+    github: 'to add',
+  })
+
+  const [scranscannerData, setScranscannerData] = useState({
+    name: 'ScranScanner',
+    img: '/scranscanner.png',
+    github: 'to add',
+  })
   
   
   return (
     <Window>
       <Welcome/>
-      <Project/>
+      <Project projectData={tracksideData}/>
+      <Project projectData={stockData}/>
+      <Project projectData={scranscannerData}/>
     </Window>
   );  
 }
 
 const Window = styled.div`
   min-height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: space-evenly;
+  
   font-family: 'Ubuntu', sans-serif;
 `
 
